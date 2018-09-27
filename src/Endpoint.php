97d13c23b50bc1d3a279464539cf6419344e5b69
Petar Španja <petar@spanja.info>
2018-09-27 09:31:32 +0200

    Introduce Endpoint

---
+++ src/Endpoint.php
@@ -0,0 +1,113 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Cabbage;
+
+use RuntimeException;
+
+/**
+ * Defines access to an Elasticsearch index.
+ */
+final class Endpoint
+{
+    /**
+     * @var string
+     */
+    public $scheme;
+
+    /**
+     * @var string
+     */
+    public $host;
+
+    /**
+     * @var int
+     */
+    public $port;
+
+    /**
+     * @var string
+     */
+    public $index;
+
+    /**
+     * @param string $scheme
+     * @param string $host
+     * @param int $port
+     * @param string $index
+     */
+    public function __construct(string $scheme, string $host, int $port, string $index)
+    {
+        $this->scheme = $scheme;
+        $this->host = $host;
+        $this->port = $port;
+        $this->index = $index;
+    }
+
+    /**
+     * Return URL of this Endpoint instance.
+     *
+     * @return string
+     */
+    public function getUrl(): string
+    {
+        return "{$this->scheme}://{$this->host}:{$this->port}/{$this->index}";
+    }
+
+    /**
+     * Build the Endpoint instance from the given $dsn.
+     *
+     * Valid DSN is in the form of 'scheme://host:port/index'.
+     *
+     * @param string $dsn
+     *
+     * @return \Cabbage\Endpoint
+     */
+    public static function fromDsn(string $dsn): self
+    {
+        $defaults = [
+            'scheme' => 'http',
+            'port' => 9200,
+        ];
+        $elements = self::parseDsn($dsn) + $defaults;
+
+        self::validateIndex($elements['path']);
+
+        return new self(
+            $elements['scheme'],
+            $elements['host'],
+            $elements['port'],
+            $elements['path']
+        );
+    }
+
+    /**
+     * @param string $dsn
+     *
+     * @return array
+     */
+    private static function parseDsn(string $dsn): array
+    {
+        $elements = parse_url(rtrim($dsn, '/'));
+
+        if ($elements === false) {
+            throw new RuntimeException(
+                'Failed to parse the given DSN'
+            );
+        }
+
+        $elements['path'] = trim($elements['path'], '/');
+
+        return $elements;
+    }
+
+    private static function validateIndex(string $index): void
+    {
+        if (\is_string($index) && \strpos($index, '/') !== false) {
+            throw new RuntimeException(
+                'Index name must not contain a slash'
+            );
+        }
+    }
+}
---
+++ tests/EndpointTest.php
@@ -0,0 +1,99 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Cabbage\Tests;
+
+use Cabbage\Endpoint;
+use PHPUnit\Framework\TestCase;
+use RuntimeException;
+
+/**
+ * @testdox Endpoint test
+ */
+class EndpointTest extends TestCase
+{
+    /**
+     * @return array|array[]
+     */
+    public function providerForBuildFromDsn(): array
+    {
+        return [
+            $dsn = 'http://localhost:9200/location' => [
+                $dsn,
+                new Endpoint(
+                    'http',
+                    'localhost',
+                    9200,
+                    'location'
+                ),
+            ],
+            $dsn = 'localhost:9200/location' => [
+                $dsn,
+                new Endpoint(
+                    'http',
+                    'localhost',
+                    9200,
+                    'location'
+                ),
+            ],
+            $dsn = 'http://localhost/location' => [
+                $dsn,
+                new Endpoint(
+                    'http',
+                    'localhost',
+                    9200,
+                    'location'
+                ),
+            ],
+        ];
+    }
+
+    /**
+     * @testdox It should build an Endpoint from a DSN
+     * @dataProvider providerForBuildFromDsn
+     *
+     * @param string $dsn
+     * @param \Cabbage\Endpoint $expectedEndpoint
+     */
+    public function testBuildFromDsn(string $dsn, Endpoint $expectedEndpoint): void
+    {
+        $this->assertEquals($expectedEndpoint, Endpoint::fromDsn($dsn));
+    }
+
+    /**
+     * @return array|array[]
+     */
+    public function providerForFailToBuildFromDsn(): array
+    {
+        return [
+            $dsn = 'localhost/location' => [
+                $dsn,
+                'Index name must not contain a slash',
+            ],
+            $dsn = 'http://localhost:abcd/location' => [
+                $dsn,
+                'Failed to parse the given DSN',
+            ],
+            $dsn = 'http://localhost/path/location' => [
+                $dsn,
+                'Index name must not contain a slash',
+            ],
+        ];
+    }
+
+    /**
+     * @testdox It should fail to build an Endpoint from a DSN
+     * @dataProvider providerForFailToBuildFromDsn
+     *
+     * @param string $dsn
+     * @param string $message
+     */
+    public function testFailToBuildFromDsn(string $dsn, string $message): void
+    {
+        $this->expectException(RuntimeException::class);
+        $this->expectExceptionMessage($message);
+
+        Endpoint::fromDsn($dsn);
+    }
+}
