40d3eaa0aca3ff20e9b1ea4da7f4e0264cd77111
Petar Španja <petar@spanja.info>
2018-10-03 19:45:51 +0200

    Use container in Handler integration tests

--- tests/Integration/HandlerTest.php
+++ tests/Integration/HandlerTest.php
@@ -4,23 +4,16 @@ declare(strict_types=1);
 
 namespace Cabbage\Tests\Integration;
 
-use Cabbage\DocumentMapper;
-use Cabbage\DocumentRouter;
-use Cabbage\DocumentSerializer;
 use Cabbage\Endpoint;
-use Cabbage\Gateway;
 use Cabbage\Handler;
-use Cabbage\Http\Client;
-use Cabbage\QueryRouter;
-use Cabbage\QueryTranslator;
-use Cabbage\ResultExtractor;
 use eZ\Publish\API\Repository\Values\Content\Query;
 use eZ\Publish\SPI\Persistence\Content;
-use Symfony\Component\DependencyInjection\Container;
-use Symfony\Component\DependencyInjection\ContainerInterface;
 
 class HandlerTest extends BaseTest
 {
+    /**
+     * @throws \Exception
+     */
     public function testIndexContent(): void
     {
         $handler = $this->getHandlerUnderTest();
@@ -35,19 +28,19 @@ class HandlerTest extends BaseTest
     /**
      * @depends testIndexContent
      *
+     * @throws \Exception
      * @throws \eZ\Publish\API\Repository\Exceptions\InvalidArgumentException
      */
     public function testFindContent(): void
     {
         $endpoint = Endpoint::fromDsn('http://localhost:9200/index');
         $handler = $this->getHandlerUnderTest();
-        $gateway = $this->getGateway();
 
         /** @var \eZ\Publish\SPI\Persistence\Content $content */
         $content = $this->getMockBuilder(Content::class)->getMock();
 
         $handler->indexContent($content);
-        $gateway->flush($endpoint);
+        $this->flush($endpoint);
 
         /** @var \eZ\Publish\API\Repository\Values\Content\Query $query */
         $query = $this->getMockBuilder(Query::class)->getMock();
@@ -57,41 +50,23 @@ class HandlerTest extends BaseTest
         $this->assertGreaterThanOrEqual(1, $searchResult->totalCount);
     }
 
-    public function getHandlerUnderTest(): Handler
+    /**
+     * @throws \Exception
+     *
+     * @return \Cabbage\Handler
+     */
+    protected function getHandlerUnderTest(): Handler
     {
-        return new Handler(
-            $this->getGateway(),
-            new DocumentMapper(),
-            new DocumentRouter(),
-            new QueryTranslator(),
-            new QueryRouter(),
-            new ResultExtractor()
-        );
-    }
-
-    public function testContainer(): void
-    {
-        $container = $this->getContainer();
-
-        $parameter = $container->getParameter('test');
-
-        $this->assertEquals('bla bla', $parameter);
+        return $this->getContainer()->get('cabbage.handler');
     }
 
     /**
-     * @var \Cabbage\Gateway
+     * @throws \Exception
+     *
+     * @param \Cabbage\Endpoint $endpoint
      */
-    protected $gateway;
-
-    protected function getGateway(): Gateway
+    protected function flush(Endpoint $endpoint): void
     {
-        if ($this->gateway === null) {
-            $this->gateway = new Gateway(
-                new Client(),
-                new DocumentSerializer()
-            );
-        }
-
-        return $this->gateway;
+        $this->getContainer()->get('cabbage.gateway')->flush($endpoint);
     }
 }
