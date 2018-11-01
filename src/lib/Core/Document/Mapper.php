a8f7d3692b13a287d0fcfc049f0d827d5d038102
Petar Španja <petar@spanja.info>
2018-11-01 09:56:42 +0100

    Extract ContentFieldMapper service

--- src/lib/Core/Document/Mapper.php
+++ src/lib/Core/Document/Mapper.php
@@ -4,18 +4,15 @@ declare(strict_types=1);
 
 namespace Cabbage\Core\Document;
 
+use Cabbage\Core\Document\Mapper\ContentFieldMapper;
 use Cabbage\SPI\Document;
 use Cabbage\SPI\Field;
 use Cabbage\SPI\FieldType\Boolean;
 use Cabbage\SPI\FieldType\Keyword;
 use eZ\Publish\SPI\Persistence\Content;
-use eZ\Publish\SPI\Persistence\Content\Field as ContentField;
 use eZ\Publish\SPI\Persistence\Content\Location;
 use eZ\Publish\SPI\Persistence\Content\Location\Handler as LocationHandler;
-use eZ\Publish\SPI\Persistence\Content\Type;
-use eZ\Publish\SPI\Persistence\Content\Type\FieldDefinition;
 use eZ\Publish\SPI\Persistence\Content\Type\Handler as TypeHandler;
-use RuntimeException;
 
 /**
  * Maps eZ Platform Content to an array of Document instances.
@@ -36,13 +33,23 @@ final class Mapper
     private $typeHandler;
 
     /**
+     * @var \Cabbage\Core\Document\Mapper\ContentFieldMapper
+     */
+    private $contentFieldMapper;
+
+    /**
      * @param \eZ\Publish\SPI\Persistence\Content\Location\Handler $locationHandler
      * @param \eZ\Publish\SPI\Persistence\Content\Type\Handler $typeHandler
+     * @param \Cabbage\Core\Document\Mapper\ContentFieldMapper $contentFieldMapper
      */
-    public function __construct(LocationHandler $locationHandler, TypeHandler $typeHandler)
-    {
+    public function __construct(
+        LocationHandler $locationHandler,
+        TypeHandler $typeHandler,
+        ContentFieldMapper $contentFieldMapper
+    ) {
         $this->locationHandler = $locationHandler;
         $this->typeHandler = $typeHandler;
+        $this->contentFieldMapper = $contentFieldMapper;
     }
 
     /**
@@ -78,8 +85,9 @@ final class Mapper
     private function mapContent(Content $content): Document
     {
         $fieldsGrouped = [[]];
+        $type = $this->typeHandler->load($content->versionInfo->contentInfo->contentTypeId);
 
-        $fieldsGrouped[] = $this->mapContentFields($content);
+        $fieldsGrouped[] = $this->contentFieldMapper->map($content, $type);
         $fieldsGrouped[] = [
             new Field('test', 'value', new Keyword()),
             new Field('test', true, new Boolean()),
@@ -108,85 +116,6 @@ final class Mapper
             "location_{$location->id}",
             Document::TypeLocation,
             $fields
-        );
-    }
-
-    /**
-     * @param \eZ\Publish\SPI\Persistence\Content $content
-     *
-     * @throws \eZ\Publish\API\Repository\Exceptions\NotFoundException
-     *
-     * @return \Cabbage\SPI\Field[]
-     */
-    private function mapContentFields(Content $content): array
-    {
-        $indexFields = [];
-
-        $type = $this->typeHandler->load($content->versionInfo->contentInfo->contentTypeId);
-        $fieldDefinitionMapById = $this->mapFieldDefinitionsById($type);
-
-        foreach ($content->fields as $field) {
-            $fieldDefinition = $this->getFieldDefinition($field, $fieldDefinitionMapById);
-
-            switch ($field->type) {
-                case 'ezboolean':
-                    $indexFields[] = new Field(
-                        $fieldDefinition->identifier,
-                        $field->value->data,
-                        new Boolean()
-                    );
-
-                    break;
-                case 'ezstring':
-                    $indexFields[] = new Field(
-                        $fieldDefinition->identifier,
-                        $field->value->data,
-                        new Keyword()
-                    );
-
-                    break;
-                default:
-                    throw new RuntimeException(
-                        "Field of type '{$field->type}' is not handled"
-                    );
-            }
-        }
-
-        return $indexFields;
-    }
-
-    /**
-     * @param \eZ\Publish\SPI\Persistence\Content\Type $type
-     *
-     * @return \eZ\Publish\SPI\Persistence\Content\Type\FieldDefinition[]
-     */
-    private function mapFieldDefinitionsById(Type $type): array
-    {
-        $map = [];
-
-        foreach ($type->fieldDefinitions as $fieldDefinition) {
-            $map[$fieldDefinition->identifier] = $fieldDefinition;
-        }
-
-        return $map;
-    }
-
-    /**
-     * @param \eZ\Publish\SPI\Persistence\Content\Field $field
-     * @param array $fieldDefinitionMapById
-     *
-     * @return \eZ\Publish\SPI\Persistence\Content\Type\FieldDefinition
-     */
-    private function getFieldDefinition(
-        ContentField $field,
-        array $fieldDefinitionMapById
-    ): FieldDefinition {
-        if (array_key_exists($field->fieldDefinitionId, $fieldDefinitionMapById)) {
-            return $fieldDefinitionMapById[$field->fieldDefinitionId];
-        }
-
-        throw new RuntimeException(
-            "Couldn't find field definition with ID '{$field->fieldDefinitionId}'"
         );
     }
 }
---
+++ src/lib/Core/Document/Mapper/ContentFieldMapper.php
@@ -0,0 +1,100 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Cabbage\Core\Document\Mapper;
+
+use Cabbage\SPI\Field;
+use Cabbage\SPI\FieldType\Boolean;
+use Cabbage\SPI\FieldType\Keyword;
+use eZ\Publish\SPI\Persistence\Content;
+use eZ\Publish\SPI\Persistence\Content\Field as ContentField;
+use eZ\Publish\SPI\Persistence\Content\Type;
+use eZ\Publish\SPI\Persistence\Content\Type\FieldDefinition;
+use RuntimeException;
+
+/**
+ * Maps content fields to Elasticsearch index fields.
+ *
+ * @see \eZ\Publish\SPI\Persistence\Content\Field
+ * @see \Cabbage\SPI\Field
+ */
+final class ContentFieldMapper
+{
+    /**
+     * @param \eZ\Publish\SPI\Persistence\Content $content
+     * @param \eZ\Publish\SPI\Persistence\Content\Type $type
+     *
+     * @return \Cabbage\SPI\Field[]
+     */
+    public function map(Content $content, Type $type): array
+    {
+        $indexFields = [];
+
+        $fieldDefinitionMapById = $this->mapFieldDefinitionsById($type);
+
+        foreach ($content->fields as $field) {
+            $fieldDefinition = $this->getFieldDefinition($field, $fieldDefinitionMapById);
+
+            switch ($field->type) {
+                case 'ezboolean':
+                    $indexFields[] = new Field(
+                        $fieldDefinition->identifier,
+                        $field->value->data,
+                        new Boolean()
+                    );
+
+                    break;
+                case 'ezstring':
+                    $indexFields[] = new Field(
+                        $fieldDefinition->identifier,
+                        $field->value->data,
+                        new Keyword()
+                    );
+
+                    break;
+                default:
+                    throw new RuntimeException(
+                        "Field of type '{$field->type}' is not handled"
+                    );
+            }
+        }
+
+        return $indexFields;
+    }
+
+    /**
+     * @param \eZ\Publish\SPI\Persistence\Content\Type $type
+     *
+     * @return \eZ\Publish\SPI\Persistence\Content\Type\FieldDefinition[]
+     */
+    private function mapFieldDefinitionsById(Type $type): array
+    {
+        $map = [];
+
+        foreach ($type->fieldDefinitions as $fieldDefinition) {
+            $map[$fieldDefinition->identifier] = $fieldDefinition;
+        }
+
+        return $map;
+    }
+
+    /**
+     * @param \eZ\Publish\SPI\Persistence\Content\Field $field
+     * @param array $fieldDefinitionMapById
+     *
+     * @return \eZ\Publish\SPI\Persistence\Content\Type\FieldDefinition
+     */
+    private function getFieldDefinition(
+        ContentField $field,
+        array $fieldDefinitionMapById
+    ): FieldDefinition {
+        if (array_key_exists($field->fieldDefinitionId, $fieldDefinitionMapById)) {
+            return $fieldDefinitionMapById[$field->fieldDefinitionId];
+        }
+
+        throw new RuntimeException(
+            "Couldn't find field definition with ID '{$field->fieldDefinitionId}'"
+        );
+    }
+}
