36e98cb26160f778b7962bc6c7f4d1fd4452c831
Petar Španja <petar@spanja.info>
2018-11-02 17:00:49 +0100

    Introduce FieldType DocumentFieldMapper

---
+++ src/lib/SPI/FieldType/DocumentFieldMapper.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Cabbage\SPI;
+
+use eZ\Publish\SPI\Persistence\Content\Field;
+use eZ\Publish\SPI\Persistence\Content\Type\FieldDefinition;
+
+/**
+ * Maps Content Field to an array of search Fields.
+ *
+ * @see \eZ\Publish\SPI\Persistence\Content\Field $field
+ * @see \eZ\Publish\SPI\Search\Field
+ */
+abstract class DocumentFieldMapper
+{
+    /**
+     * @param \eZ\Publish\SPI\Persistence\Content\Field $field
+     * @param \eZ\Publish\SPI\Persistence\Content\Type\FieldDefinition $fieldDefinition
+     *
+     * @return \eZ\Publish\SPI\Search\Field[]
+     */
+    abstract public function map(Field $field, FieldDefinition $fieldDefinition): array;
+}
