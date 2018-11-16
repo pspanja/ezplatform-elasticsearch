3d672239b9a8815b7b89c3eb2f0e18f22aa14219
Petar Španja <petar@spanja.info>
2018-11-16 19:28:55 +0100

    Implement ContentId criterion visitor

---
+++ src/lib/Core/Query/Translator/Criterion/Visitor/ContentId.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Cabbage\Core\Query\Translator\Criterion\Visitor;
+
+use Cabbage\Core\Query\Translator\Criterion\Visitor;
+use Cabbage\Core\Query\Translator\Criterion\Converter;
+use eZ\Publish\API\Repository\Values\Content\Query\Criterion;
+use eZ\Publish\API\Repository\Values\Content\Query\Criterion\ContentId as ContentIdCriterion;
+
+/**
+ * Visits ContentId criterion.
+ *
+ * @see \eZ\Publish\API\Repository\Values\Content\Query\Criterion\ContentId
+ */
+final class ContentId extends Visitor
+{
+    public function accept(Criterion $criterion): bool
+    {
+        return $criterion instanceof ContentIdCriterion;
+    }
+
+    public function visit(Criterion $criterion, Converter $converter): array
+    {
+        return [
+            'term' => [
+                'content_id' => $criterion->value[0],
+            ],
+        ];
+    }
+}
