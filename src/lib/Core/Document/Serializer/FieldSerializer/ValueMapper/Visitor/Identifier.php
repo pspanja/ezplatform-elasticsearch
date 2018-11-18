83ae185005d0948288dfa03aad62f238a7d99e86
Petar Španja <petar@spanja.info>
2018-11-18 15:53:03 +0100

    Implement Identifier document field type value mapper

---
+++ src/lib/Core/Document/Serializer/FieldSerializer/ValueMapper/Visitor/Identifier.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Cabbage\Core\Document\Serializer\FieldSerializer\ValueMapper\Visitor;
+
+use Cabbage\Core\Document\Serializer\FieldSerializer\ValueMapper\Visitor;
+use Cabbage\SPI\Document\Field;
+use Cabbage\SPI\Document\Field\Type\Identifier as IdentifierType;
+
+class Identifier extends Visitor
+{
+    public function accept(Field $field): bool
+    {
+        return $field->type instanceof IdentifierType;
+    }
+
+    public function visit(Field $field)
+    {
+        return (string)$field->value;
+    }
+}
