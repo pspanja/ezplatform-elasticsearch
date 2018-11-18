1b4d721e6bba5534ff7a60099681111ffdad368f
Petar Španja <petar@spanja.info>
2018-11-18 15:52:17 +0100

    Extract real ContentInfo and Location

--- src/lib/Core/ResultExtractor.php
+++ src/lib/Core/ResultExtractor.php
@@ -6,6 +6,10 @@ namespace Cabbage\Core;
 
 use eZ\Publish\API\Repository\Values\Content\Search\SearchHit;
 use eZ\Publish\API\Repository\Values\Content\Search\SearchResult;
+use eZ\Publish\API\Repository\Values\ValueObject;
+use eZ\Publish\SPI\Persistence\Content\ContentInfo;
+use eZ\Publish\SPI\Persistence\Content\Location;
+use RuntimeException;
 
 /**
  * Extracts search result from the raw response string.
@@ -25,12 +29,50 @@ final class ResultExtractor
         $searchHits = [];
 
         foreach ($body->hits->hits as $hit) {
-            $searchHits[] = new SearchHit(['valueObject' => $hit]);
+            $searchHits[] = new SearchHit([
+                'valueObject' => $this->extractSearchHit($hit),
+            ]);
         }
 
         return new SearchResult([
             'searchHits' => $searchHits,
             'totalCount' => $body->hits->total,
+        ]);
+    }
+
+    /**
+     * @see https://www.elastic.co/guide/en/elasticsearch/reference/current/_the_search_api.html
+     *
+     * @param object $hit
+     *
+     * @return \eZ\Publish\API\Repository\Values\ValueObject
+     */
+    private function extractSearchHit(object $hit): ValueObject
+    {
+        if ($hit->_source->type === 'content') {
+            return $this->extractContentInfo($hit);
+        }
+
+        if ($hit->_source->type === 'location') {
+            return $this->extractLocation($hit);
+        }
+
+        throw new RuntimeException(
+            "Document of type '{$hit->_source->type}' is not handled"
+        );
+    }
+
+    private function extractContentInfo(object $hit): ContentInfo
+    {
+        return new ContentInfo([
+            'id' => $hit->_source->content_id_identifier,
+        ]);
+    }
+
+    private function extractLocation(object $hit): Location
+    {
+        return new Location([
+            'id' => $hit->_source->location_id_identifier,
         ]);
     }
 }
