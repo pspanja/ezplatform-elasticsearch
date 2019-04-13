060108c0a8df731d27c3a151031502c957db7592
Petar Španja <petar@spanja.info>
2019-04-13 10:10:18 +0200

    Introduce Indexer

--- src/lib/Core/Handler.php
+++ src/lib/Core/Handler.php
@@ -4,12 +4,8 @@ declare(strict_types=1);
 
 namespace Cabbage\Core;
 
-use Cabbage\Core\Document\Mapper;
-use Cabbage\Core\Document\Serializer;
 use Cabbage\Core\Query\TargetResolver;
 use Cabbage\Core\Query\Translator;
-use Cabbage\SPI\Index;
-use Cabbage\SPI\Node;
 use eZ\Publish\API\Repository\Values\Content\LocationQuery;
 use eZ\Publish\API\Repository\Values\Content\Query;
 use eZ\Publish\API\Repository\Values\Content\Query\Criterion;
@@ -24,24 +20,14 @@ use RuntimeException;
 final class Handler implements HandlerInterface, Capable
 {
     /**
+     * @var \Cabbage\Core\Indexer
+     */
+    private $indexer;
+
+    /**
      * @var \Cabbage\Core\Gateway
      */
     private $gateway;
-
-    /**
-     * @var \Cabbage\Core\Document\Mapper
-     */
-    private $documentMapper;
-
-    /**
-     * @var \Cabbage\Core\Document\Serializer
-     */
-    private $documentSerializer;
-
-    /**
-     * @var \Cabbage\Core\IndexRegistry
-     */
-    private $indexRegistry;
 
     /**
      * @var \Cabbage\Core\Query\Translator
@@ -59,29 +45,23 @@ final class Handler implements HandlerInterface, Capable
     private $resultExtractor;
 
     /**
+     * @param \Cabbage\Core\Indexer $indexer
      * @param \Cabbage\Core\Gateway $gateway
-     * @param \Cabbage\Core\Document\Mapper $documentMapper
-     * @param \Cabbage\Core\Document\Serializer $documentSerializer
      * @param \Cabbage\Core\Query\Translator $queryTranslator
      * @param \Cabbage\Core\Query\TargetResolver $targetResolver
-     * @param \Cabbage\Core\IndexRegistry $indexRegistry
      * @param \Cabbage\Core\ResultExtractor $resultExtractor
      */
     public function __construct(
+        Indexer $indexer,
         Gateway $gateway,
-        Mapper $documentMapper,
-        Serializer $documentSerializer,
         Translator $queryTranslator,
         TargetResolver $targetResolver,
-        IndexRegistry $indexRegistry,
         ResultExtractor $resultExtractor
     ) {
+        $this->indexer = $indexer;
         $this->gateway = $gateway;
-        $this->documentMapper = $documentMapper;
-        $this->documentSerializer = $documentSerializer;
         $this->queryTranslator = $queryTranslator;
         $this->targetResolver = $targetResolver;
-        $this->indexRegistry = $indexRegistry;
         $this->resultExtractor = $resultExtractor;
     }
 
@@ -144,15 +124,7 @@ final class Handler implements HandlerInterface, Capable
      */
     public function indexContent(Content $content): void
     {
-        $this->gateway->index(
-            new Index(
-                Node::fromDsn('http://localhost:9200'),
-                'index'
-            ),
-            $this->documentSerializer->serialize(
-                $this->documentMapper->map($content)
-            )
-        );
+        $this->indexer->indexContent($content);
     }
 
     /**
@@ -160,12 +132,12 @@ final class Handler implements HandlerInterface, Capable
      */
     public function deleteContent($contentId, $versionId = null): void
     {
-        throw new RuntimeException('Not implemented');
+        $this->indexer->deleteContent($contentId, $versionId);
     }
 
     public function indexLocation(Location $location): void
     {
-        // Does nothing
+        $this->indexer->indexLocation($location);
     }
 
     /**
@@ -173,17 +145,12 @@ final class Handler implements HandlerInterface, Capable
      */
     public function deleteLocation($locationId, $contentId): void
     {
-        throw new RuntimeException('Not implemented');
+        $this->indexer->deleteLocation($locationId, $contentId);
     }
 
     public function purgeIndex(): void
     {
-        $this->gateway->purge(
-            new Index(
-                Node::fromDsn('http://localhost:9200'),
-                'index'
-            )
-        );
+        $this->indexer->purgeIndex();
     }
 
     /**
@@ -193,40 +160,16 @@ final class Handler implements HandlerInterface, Capable
      */
     public function bulkIndexContent(array $contentItems): void
     {
-        $payload = '';
-
-        foreach ($contentItems as $content) {
-            $payload .= $this->documentSerializer->serialize(
-                $this->documentMapper->map($content)
-            );
-        }
-
-        $this->gateway->index(
-            new Index(
-                Node::fromDsn('http://localhost:9200'),
-                'index'
-            ),
-            $payload
-        );
+        $this->indexer->bulkIndexContent($contentItems);
     }
 
     public function flush(): void
     {
-        $this->gateway->flush(
-            new Index(
-                Node::fromDsn('http://localhost:9200'),
-                'index'
-            )
-        );
+        $this->indexer->flush();
     }
 
     public function refresh(): void
     {
-        $this->gateway->refresh(
-            new Index(
-                Node::fromDsn('http://localhost:9200'),
-                'index'
-            )
-        );
+        $this->indexer->refresh();
     }
 }
---
+++ src/lib/Core/Indexer.php
@@ -0,0 +1,145 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Cabbage\Core;
+
+use Cabbage\Core\Document\Mapper;
+use Cabbage\Core\Document\Serializer;
+use Cabbage\SPI\Index;
+use Cabbage\SPI\Node;
+use eZ\Publish\SPI\Persistence\Content;
+use eZ\Publish\SPI\Persistence\Content\Location;
+use RuntimeException;
+
+final class Indexer
+{
+    /**
+     * @var \Cabbage\Core\Gateway
+     */
+    private $gateway;
+
+    /**
+     * @var \Cabbage\Core\Document\Mapper
+     */
+    private $documentMapper;
+
+    /**
+     * @var \Cabbage\Core\Document\Serializer
+     */
+    private $documentSerializer;
+
+    /**
+     * @param \Cabbage\Core\Gateway $gateway
+     * @param \Cabbage\Core\Document\Mapper $documentMapper
+     * @param \Cabbage\Core\Document\Serializer $documentSerializer
+     */
+    public function __construct(
+        Gateway $gateway,
+        Mapper $documentMapper,
+        Serializer $documentSerializer
+    ) {
+        $this->gateway = $gateway;
+        $this->documentMapper = $documentMapper;
+        $this->documentSerializer = $documentSerializer;
+    }
+
+    /**
+     * {@inheritdoc}
+     *
+     * @throws \eZ\Publish\API\Repository\Exceptions\NotFoundException
+     */
+    public function indexContent(Content $content): void
+    {
+        $this->gateway->index(
+            new Index(
+                Node::fromDsn('http://localhost:9200'),
+                'index'
+            ),
+            $this->documentSerializer->serialize(
+                $this->documentMapper->map($content)
+            )
+        );
+    }
+
+    /**
+     * Deletes a content object from the index.
+     *
+     * @param int $contentId
+     * @param int|null $versionId
+     */
+    public function deleteContent($contentId, $versionId = null): void
+    {
+        throw new RuntimeException('Not implemented');
+    }
+
+    public function indexLocation(Location $location): void
+    {
+        // Does nothing
+    }
+
+    /**
+     * Deletes a location from the index.
+     *
+     * @param mixed $locationId
+     * @param mixed $contentId
+     */
+    public function deleteLocation($locationId, $contentId): void
+    {
+        throw new RuntimeException('Not implemented');
+    }
+
+    public function purgeIndex(): void
+    {
+        $this->gateway->purge(
+            new Index(
+                Node::fromDsn('http://localhost:9200'),
+                'index'
+            )
+        );
+    }
+
+    /**
+     * @param \eZ\Publish\SPI\Persistence\Content[] $contentItems
+     *
+     * @throws \eZ\Publish\API\Repository\Exceptions\NotFoundException
+     */
+    public function bulkIndexContent(array $contentItems): void
+    {
+        $payload = '';
+
+        foreach ($contentItems as $content) {
+            $payload .= $this->documentSerializer->serialize(
+                $this->documentMapper->map($content)
+            );
+        }
+
+        $this->gateway->index(
+            new Index(
+                Node::fromDsn('http://localhost:9200'),
+                'index'
+            ),
+            $payload
+        );
+    }
+
+    public function flush(): void
+    {
+        $this->gateway->flush(
+            new Index(
+                Node::fromDsn('http://localhost:9200'),
+                'index'
+            )
+        );
+    }
+
+    public function refresh(): void
+    {
+        $this->gateway->refresh(
+            new Index(
+                Node::fromDsn('http://localhost:9200'),
+                'index'
+            )
+        );
+    }
+}
