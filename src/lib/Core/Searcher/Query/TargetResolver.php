b7085179658a144557a51d82949bc23abd80c9f0
Petar Španja <petar@spanja.info>
2019-04-22 09:18:13 +0200

    Remove unused imports

--- src/lib/Core/Searcher/Query/TargetResolver.php
+++ src/lib/Core/Searcher/Query/TargetResolver.php
@@ -5,8 +5,6 @@ declare(strict_types=1);
 namespace Cabbage\Core\Searcher\Query;
 
 use Cabbage\Core\IndexRegistry;
-use Cabbage\SPI\Index;
-use eZ\Publish\API\Repository\Values\Content\Query;
 
 /**
  * Matches a query to a Target.
