8dbebe69822bfc289181f72d07ee4de7f8cb6172
Petar Španja <petar@spanja.info>
2019-05-12 07:01:32 +0200

    Move HttpClient test to match implementation

--- tests/Integration/Core/HttpClient/ClientTest.php
+++ tests/Integration/Core/HttpClient/ClientTest.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace Cabbage\Tests\Integration\Core\Http;
+namespace Cabbage\Tests\Integration\Core\HttpClient;
 
 use Cabbage\Core\HttpClient\Client;
 use Cabbage\Core\HttpClient\ConnectionException;
